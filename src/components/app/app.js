e1dfba779d0794be9c0cd14b45092086ae922fb8
Cody Maverick <rocknroll8905@gmail.com>
2019-10-23 10:55:41 +0300

    first commit

---
+++ src/components/app/app.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+import './app.less';
+
+export default class App extends Component {
+    render() {
+        return (
+            <div className="app">
+                <h1 className="app__title">
+                    REACT TITLE
+                </h1>
+            </div>
+        )
+    }
+}
---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: "./src/index.js",
+    output: {
+        path: path.join(__dirname, "/public"),
+        filename: "index_bundle.js"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                },
+            },
+            {
+                test: /\.less$/,
+                use: [{
+                    loader: 'style-loader' // creates style nodes from JS strings
+                }, {
+                    loader: 'css-loader' // translates CSS into CommonJS
+                }, {
+                    loader: 'less-loader' // compiles Less to CSS
+                }]
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./src/index.html"
+        })
+    ]
+};
